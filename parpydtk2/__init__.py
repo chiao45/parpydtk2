438befde4ab4506058bfd5b53829a28eeb608ad0
Qiao Chen <benechiao@gmail.com>
2018-09-05 23:16:59 -0400

    updated init file, added doc strings, added get_include

--- parpydtk2/__init__.py
+++ parpydtk2/__init__.py
@@ -1,29 +1,72 @@
+"""Main module interface of ParPyDTK2
+
+Attributes
+----------
+B2G : bool
+    boolean flag of ``True`` denotes transferring direction from ``blue`` to
+    ``green``
+G2B : bool
+    boolean flag of ``False`` denotes transferring direction from ``green`` to
+    ``blue``
+MMLS : int
+    flag (0) represents using `modified moving least square` method
+SPLINE : int
+    flag (1) represents using `spline interpolation` method
+N2N : int
+    flag (2) represents using `nearest node projection` method
+WENDLAND2 : int
+    flag (0) represents using `Wendland 2nd-order` RBF weights
+WENDLAND4 : int
+    flag (1) represents using `Wendland 4th-order` RBF weights
+WENDLAND6 : int
+    flag (2) represents using `Wendland 6th-order` RBF weights
+WU2 : int
+    flag (3) represents using `Wu 2nd-order` RBF weights
+WU4 : int
+    flag (4) represents using `Wu 4th-order` RBF weights
+WU6 : int
+    flag (5) represents using `Wu 6th-order` RBF weights
+BUHMANN3 : int
+    flag (6) represents using `Buhmann 3rd-order` RBF weights
+"""
+
 import sys
+import parpydtk2.error_handle as error
 from ._version import __version__
+from .imeshdb import IMeshDB
 from .mapper import Mapper
 
+__b2g__ = True
+__g2b__ = False
+B2G = __b2g__
+G2B = __g2b__
 
-def _excepthook(exctype, value, tb):
-    # override default excepthook
-    sys.__excepthook__(exctype, value, tb)
-    from mpi4py import MPI
-    if MPI.Is_initialized() and MPI.COMM_WORLD.size > 1:
-        MPI.COMM_WORLD.Abort(1)
+__mmls__ = 0
+__spline__ = 1
+__n2n__ = 2
+MMLS = __mmls__
+SPLINE = __spline__
+N2N = __n2n__
+
+__wendland2__ = 0
+__wendland4__ = 1
+__wendland6__ = 2
+WENDLAND2 = __wendland2__
+WENDLAND4 = __wendland4__
+WENDLAND6 = __wendland6__
+
+__wu2__ = 3
+__wu4__ = 4
+__wu6__ = 5
+WU2 = __wu2__
+WU4 = __wu4__
+WU6 = __wu6__
+
+__buhmann3__ = 6
+BUHMANN3 = __buhmann3__
 
 
-sys.excepthook = _excepthook
-
-B2G = True
-G2B = False
-
-MMLS = 0
-SPLINE = 1
-N2N = 2
-
-WENDLAND2 = 0
-WENDLAND4 = 1
-WENDLAND6 = 2
-WU2 = 3
-WU4 = 4
-WU6 = 5
-BUHMANN3 = 6
+def get_include():
+    """Get the abs include path"""
+    import os
+    return os.path.dirname(os.path.abspath(__file__))
